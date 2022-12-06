b455e2bcb737a34a5a263fd8086dcdae849c71ff
n1ck-nam3 <50087686+n1ck-nam3@users.noreply.github.com>
2022-12-05 21:20:34 -0400

    created notes

---
+++ notes.py
@@ -0,0 +1,30 @@
+## concatenation and adding spaces ##
+#print('hello' + " " + "Nick")
+
+## moving a string to another line with \n ##
+#print('hello world\nnah mean?')
+
+## asks for name and says hello to the input ##
+#print ('hello ' + input('What is your name?'))
+
+## calculates number of letters in an inputted name ##
+#print(len(input("What is your Name? ")))
+
+#name = input('What is your name?')
+#length = len(name)
+#print(length)
+
+## Data Types
+    ## String subscripting, start from 0, select character from a string
+#print("Hello"[0])
+#print("12345"[0])
+
+    ## Integer
+#print(1+1)
+
+    ## Float
+#print(3.149)
+
+    ## Boolean
+#print(1==1)
+#print(1==2)
